fe940a75bad87ab37c055cdda8fd8e4303e034ea
Prasan32 <prasannakb440@gmail.com>
2020-09-23 21:04:56 +0545

    add cart,auth

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const layout = require('express-ejs-layouts')
 const path=require('path')
 
 //assets
-app.use(express.static('public'))
-
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+app.use(express.static('public'));
 
 //set templating engine
 app.use(layout);
@@ -18,6 +14,20 @@ app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
 app.listen(PORT,(error,result)=>{
     console.log(`server is listening at ${PORT}`)
